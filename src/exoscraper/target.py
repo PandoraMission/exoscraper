be8365e24da43cd08562a36ddf177cd336e2c2fc
benhord <benjhord@gmail.com>
2023-06-13 13:50:34 -0400

    started sketching out functionality

--- src/exoscraper/target.py
+++ src/exoscraper/target.py
@@ -37,7 +37,49 @@ class Target(object):
         # go get the TESS data something like
         # return get_timeseries(self.ra, self.dec)
         raise NotImplementedError
+
+    @property
+    def bibliography(self):
+        # go get references from NASA ADS
+        # return get_bibliography(self.name)
+        raise NotImplementedError
+
+    @property
+    def transit_times(self, time: u.Quantity):
+        # calculate future transit times
+        # return array of future transit times out to a specified time
+        raise NotImplementedError
+
+    @property
+    def SED(self):
+        # calculate SED of target star
+        # return idk whatever Christina has written
+        raise NotImplementedError
+
+    @property
+    def transit_model(self) -> lk.LightCurve:
+        # generate transit model using lightkurve
+        # return LightCurve object
+        raise NotImplementedError
+
+    @property
+    def noise_model(self, mission: str) -> lk.LightCurve:
+        # generate noise model for target for specified mission
+        # this could be wrapped into transit model
+        # this also may not be worth including since I selfishly need it for
+        #  pandora-target and so added it here lol
+        # returns LightCurve object
+        raise NotImplementedError
     
+    def motto(self):
+        # mottos = ["If it's online, we can scrape it!",
+        #           "Leave the scraping to us!",
+        #           "Scraping the internet clean!",
+        #           "Scrape it or leave it!",
+        #           "Consider it scraped!"]
+
+        # print(random.choice(mottos))
+        raise NotImplementedError
 
 class TargetSet(object):
     """A class to hold many Target classes"""
