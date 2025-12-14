045761cef8f4930c356652f44a9a9f7715c7bfbe
benhord <benjhord@gmail.com>
2025-12-13 19:51:22 -0500

    Added separation calculation utility

--- src/exoscraper/utils.py
+++ src/exoscraper/utils.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 import batman
+import pandas as pd
+
+from gaiaoffline import Gaia, populate_gaiadr3, populate_tmass, populate_tmass_xmatch
 
 from .query import get_citation
 
@@ -56,3 +59,56 @@ def get_batman_model(
         return model, params
     else:
         return model
+
+
+def handle_gaiaoffline_files():
+    """
+    Function to check the local `gaiaoffline` install and add the relevant files if necessary.
+    """
+    tracker_table_names = Gaia().file_tracker_table_names
+    track_fracs = {}
+    for tracker_table_name in tracker_table_names:
+        df = pd.read_sql_query(
+            f"""SELECT * FROM {tracker_table_name}""", Gaia().conn
+        )
+        frac = df.status.isin(["completed"]).sum() / len(df)
+        track_fracs.update({str(tracker_table_name): frac})
+
+    if track_fracs['file_tracking_gaiadr3'] < 1.0:
+        print('Warning: This may take a while.')
+        print('Populating Gaia DR3 files for gaiaoffline')
+        populate_gaiadr3()
+
+    if track_fracs['file_tracking_tmass_xmatch'] < 1.0:
+        print('Warning: This may take a while.')
+        print('Populating 2MASS Xmatch files for gaiaoffline')
+        populate_tmass_xmatch()
+
+    if track_fracs['file_tracking_tmass'] < 1.0:
+        print('Warning: This may take a while.')
+        print('Populating 2MASS files for gaiaoffline')
+        populate_tmass()
+
+
+def calc_separation(ref_ra, ref_dec, ra, dec):
+    """
+    Function to calculate the separation between two RA/Dec coordinate sets.
+
+    ref_ra: float
+        RA of reference coordinate set.
+    ref_dec: float
+        Dec of reference coordinate set.
+    ra: float
+        RA of target coordinate set.
+    dec: float
+        Dec of target coordinate set.
+    """
+    ra1 = np.radians(ref_ra).astype(np.float64)
+    ra2 = np.radians(ra).astype(np.float64)
+    dec1 = np.radians(ref_dec).astype(np.float64)
+    dec2 = np.radians(dec).astype(np.float64)
+
+    numerator = np.sin((dec2 - dec1) / 2) ** 2 + np.cos(dec1) * np.cos(dec2) * np.sin((ra2 - ra1) / 2) ** 2
+    sep = 2 * np.arctan2(numerator ** 0.5, (1 - numerator) ** 0.5)
+
+    return sep
